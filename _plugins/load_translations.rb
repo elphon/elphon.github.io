110f0c10bf2f49abe529e58cabba0b60b212b4b4
elphon <56273030+elphon@users.noreply.github.com>
2024-12-24 23:47:49 +0900

    포스트에 디버깅 실행행

--- _plugins/load_translations.rb
+++ _plugins/load_translations.rb
@@ -1,13 +1,13 @@
-require 'yaml'
+# require 'yaml'
 
-module Jekyll
-  class LoadTranslations < Generator
-    def generate(site)
-      translations_path = File.join(site.source, 'src/yml/translations.yml')
-      if File.exist?(translations_path)
-        translations = YAML.load_file(translations_path)
-        site.data['translations'] = translations
-      end
-    end
-  end
-end
+# module Jekyll
+#   class LoadTranslations < Generator
+#     def generate(site)
+#       translations_path = File.join(site.source, 'src/yml/translations.yml')
+#       if File.exist?(translations_path)
+#         translations = YAML.load_file(translations_path)
+#         site.data['translations'] = translations
+#       end
+#     end
+#   end
+# end
